33fce92994e4c9e01c1bf4f4f669565581d6d727
Auke Roorda <aukeroorda@gmail.com>
2023-09-10 17:44:28 +0200

    Added script, but more importantly a note to use the given project construction function

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 Base project to get started with Rascal in VS code
 
 
+__Note__: Instead of this script, use [https://www.rascal-mpl.org/docs/GettingStarted/CreateNewProject/](https://www.rascal-mpl.org/docs/GettingStarted/CreateNewProject/)
+
+
 ## Setup
 This Rascal project is setup by the commands found in `init.sh`
 
--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ mvn archetype:generate \
     -DarchetypeVersion=1.4 \
     -DinteractiveMode=false
 
-# Continue working in the `my-app` directory
+# Continue working in the `my-app` directory.
 (
   cd my-app
 
@@ -23,4 +23,25 @@ Require-Libraries: |lib://rascal-lsp|
 
 
 " > RASCAL.MF
+
+  # Move out of the META-INF folder
+  cd ..
+
+  # I think (but am unsure) that in general the Rascal code should reside under `src/main/rascal`, but I could be wrong in this regard. Note that this is also the path that we gave for the `Source` field in the RASCAL.MF config.
+  # Construct this directory
+  mkdir -p src/main/rascal 
+
+  # Construct a Rascal source file
+  echo "module Syntax
+
+extend lang::std::Layout;
+extend lang::std::Id;
+
+start syntax Machine = machine: State+ states;
+syntax State = @Foldable state: "state" Id name Trans* out;
+syntax Trans = trans: Id event ":" Id to;" > src/main/rascal/Syntax.rsc
+
+  # You should now be able to import this module in a Rascal console in either of two ways:
+    # Click Import in new Rascal terminal at the top of the Syntax.rsc source file
+    # or Open a Rascal Terminal yourself (cmd/ctrl + shift + P > Create Rascal Terminal), and import using import Syntax;.
 )
